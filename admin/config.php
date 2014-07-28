c037d4e52562a772ccd973272f8d779e495e3014
yurii <yuriikrevnyi@gmail.com>
2014-07-28 17:59:40 +0300

    Fresh opencart install

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart-clear.dev/admin/');
+define('HTTP_CATALOG', 'http://opencart-clear.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart-clear.dev/admin/');
+define('HTTPS_CATALOG', 'http://opencart-clear.dev/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\opencart-clear.dev\www/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\opencart-clear.dev\www/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\opencart-clear.dev\www/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\opencart-clear.dev\www/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\opencart-clear.dev\www/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\opencart-clear.dev\www/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\opencart-clear.dev\www/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\opencart-clear.dev\www/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\opencart-clear.dev\www/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\opencart-clear.dev\www/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\opencart-clear.dev\www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_watermark_module');
+define('DB_PREFIX', '');
+?>
