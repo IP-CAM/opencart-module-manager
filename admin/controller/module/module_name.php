f10688a96a9d65a4536f738d87cacada2ce24201
yurii <yuriikrevnyi@gmail.com>
2014-07-31 15:16:53 +0300

    Add some default files

---
+++ admin/controller/module/module_name.php
@@ -0,0 +1,95 @@
+<?php
+
+class ControllerModuleModuleName extends Controller {
+
+	public function index() {
+		$this->load->model('tool/image');
+
+		$this->language->load('module/module_name');
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			// Store module settings
+		}
+
+		// Set up breadcrumbs
+		$this->data['breadcrumbs'] = array();
+
+		$this->data['breadcrumbs'][] = array(
+			'text'      => $this->language->get('text_home'),
+			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+			'separator' => false
+		);
+
+		$this->data['breadcrumbs'][] = array(
+			'text'      => $this->language->get('text_module'),
+			'href'      => $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL'),
+			'separator' => ' :: '
+		);
+
+		$this->data['breadcrumbs'][] = array(
+			'text'      => $this->language->get('heading_title'),
+			'href'      => $this->url->link('module/module_name', 'token=' . $this->session->data['token'], 'SSL'),
+			'separator' => ' :: '
+		);
+
+		// Language
+		$this->data['heading_title'] = $this->language->get('heading_title');
+
+		// Local vars
+		$this->data['action'] = $this->url->link('module/watermark', 'token=' . $this->session->data['token'], 'SSL');
+		$this->data['cancel'] = $this->url->link('extension/module', 'token=' . $this->session->data['token'], 'SSL');
+		$this->data['token'] = $this->session->data['token'];
+		$this->data['no_image'] = $this->model_tool_image->resize('no_image.jpg', 100, 100);
+
+		$this->template = 'teil/module/module_name.tpl';
+		$this->children = array(
+			'common/header',
+			'common/footer'
+		);
+
+		$this->response->setOutput($this->render());
+	}
+
+
+	// Get module setting
+	public function settings()
+	{
+		$this->load->model('tool/image');
+		$this->load->model('setting/setting');
+		$this->language->load('module/module_name');
+		
+		$result = $this->model_setting_setting->getSetting('module_name');
+
+		// Get image
+		// if (!file_exists(DIR_IMAGE . $result['image']) AND !is_file(DIR_IMAGE . $result['image']))
+		// {
+		// 	$result['image'] = $this->model_tool_image->resize('no_image.jpg', 100, 100);
+		// }
+		// else
+		// {
+		// 	$result['image'] = $this->model_tool_image->resize($result['image'], 100, 100);
+		// }
+
+		echo json_encode($result); die();
+	}
+
+
+	// Store module config in the database
+	public function store()
+	{
+		$result = array('status' => false);
+
+		if ($this->request->server['REQUEST_METHOD'] == 'POST')
+		{
+			// Store module settings
+			// $this->load->model('setting/setting');
+			// $this->model_setting_setting->editSetting('watermark', $this->request->post);
+
+			$result['status'] = true;
+		}
+
+		echo json_encode($result); die();
+	}
+
+}
